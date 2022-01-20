1720cca4aec5cfda06e7ef9452123205ec7c551a
Neskol <hs-neskol@outlook.com>
2022-01-20 15:27:37 -0500

    Update Simaiparser.cs
    Add template to compile on

--- Simaiparser.cs
+++ Simaiparser.cs
@@ -2,5 +2,38 @@ namespace MaidataConverter;
 
 public class Simaiparser : IParser
 {
+    public BPMChanges BPMChangesOfToken(string token)
+    {
+        throw new NotImplementedException();
+    }
 
+    public GoodBrother1 GoodBrotherOfToken(string[] token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Hold HoldOfToken(string token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public MeasureChanges MeasureChangesOfToken(string token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Note NoteOfToken(string token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Slide SlideOfToken(string token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Tap TapOfToken(string token)
+    {
+        throw new NotImplementedException();
+    }
 }
