dff7ddb42ec538e295e6ab86722289e814298b23
neskol <hs-neskol@outlook.com>
2022-01-25 13:21:17 -0500

    Changed a bit dependency

---
+++ IChart.cs
@@ -0,0 +1,22 @@
+﻿namespace MaidataConverter
+{
+    interface IChart
+    {
+        /// <summary>
+        /// Updates all information
+        /// </summary>
+        void Update();
+
+        /// <summary>
+        /// Check if this Good Brother is valid
+        /// </summary>
+        /// <returns></returns>
+        bool CheckValidity();
+
+        /// <summary>
+        /// Export this Good Brother
+        /// </summary>
+        /// <returns></returns>
+        string Compose();
+    }
+}
