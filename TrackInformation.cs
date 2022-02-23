5120ffc1104d6cc766ac50b80e467a56d04be50e
neskol <hs-neskol@outlook.com>
2022-02-23 13:03:16 -0500

    UNiVERSE Ready

--- TrackInformation.cs
+++ TrackInformation.cs
@@ -17,7 +17,7 @@ namespace MaichartConverter
         /// Stores prover maimai versions
         /// </summary>
         /// <value>Version name of each generation of Maimai</value>
-        public static readonly string[] version = { "maimai", "maimai PLUS", "maimai GreeN", "maimai GreeN PLUS", "maimai ORANGE", "maimai ORANGE PLUS", "maimai PiNK", "maimai PiNK PLUS", "maimai MURASAKi", "maimai MURASAKi PLUS", "maimai MiLK", "maimai MiLK PLUS", "maimai FiNALE", "maimai DX", "maimai DX PLUS", "maimai DX Splash", "maimai DX Splash PLUS", "maimai UNiVERSE" };
+        public static readonly string[] version = { "maimai", "maimai PLUS", "maimai GreeN", "maimai GreeN PLUS", "maimai ORANGE", "maimai ORANGE PLUS", "maimai PiNK", "maimai PiNK PLUS", "maimai MURASAKi", "maimai MURASAKi PLUS", "maimai MiLK", "maimai MiLK PLUS", "maimai FiNALE", "maimai DX", "maimai DX PLUS", "maimai DX Splash", "maimai DX Splash PLUS", "maimai DX UNiVERSE" };
 
         /// <summary>
         /// Set of track information stored
