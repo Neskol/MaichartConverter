9d34715dff08d6cb8c2181c5c50745e5d9f0659c
neskol <hs-neskol@outlook.com>
2022-03-08 16:17:39 -0500

    Start working on SimaiParser

--- Simaiparser.cs
+++ Simaiparser.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
+
 namespace MaichartConverter;
 
 public class SimaiParser : IParser
 {
     public readonly string[] State = { "Note","Tap","Break","Touch","EXTap","Slide","Hold","EXHold","TouchHold","BPM","Quaver","Information" };
     public readonly string[] Status = { "Ready","Submit"};
+
+    public static int MaximumDefinition = 384;
     /// <summary>
     /// Constructor of simaiparser
     /// </summary>
@@ -16,14 +20,48 @@ public class SimaiParser : IParser
         throw new NotImplementedException();
     }
 
-    public Chart ChartOfToken(string[] token)
+    public Chart ChartOfToken(string[] tokens)
     {
         throw new NotImplementedException();
         Chart result = new Simai2();
 
-        foreach (string block in token)
+        List<Note> notes = new List<Note>();
+        BPMChanges bpmChanges = new BPMChanges();
+        MeasureChanges measureChanges = new MeasureChanges();
+        int bar=0;
+        int tick=0;
+        int tickStep=MaximumDefinition;
+        for (int i = 0;i<tokens.Length;i++)
         {
-            
+            bool isBPM = tokens[i].Contains("(");
+            bool isMeasure = tokens[i].Contains("{");
+            bool ended = tokens[i].Equals("E");
+
+            if (isBPM)
+            {
+                string bpm = tokens[i];
+                bpm.Replace("(","");
+                bpm.Replace(")","");
+                bpmChanges.Add(new BPMChange(bar,tick,Double.Parse(bpm)));
+            }
+            else if (isMeasure)
+            {
+                string quaverCandidate = tokens[i];
+                quaverCandidate.Replace("{","");
+                quaverCandidate.Replace("}","");
+                tickStep = MaximumDefinition/Int32.Parse(quaverCandidate);
+            }
+            else
+            {
+                notes.Add(NoteOfToken(tokens[i]));
+            }
+
+            tick+=tickStep;
+            while (tick>=MaximumDefinition)
+            {
+                tick-=MaximumDefinition;
+                bar++;
+            }
         }
 
         return result;
